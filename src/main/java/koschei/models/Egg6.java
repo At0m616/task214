b6303fd57909479c2e5d358c7b4d4ab70f3b4516
Artem Kuznetsov <at0m616@yandex.ru>
2021-06-23 22:10:44 +0500

    Update Configuration

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -4,9 +4,11 @@ package koschei.models;
 public class Egg6 {
     private Needle7 needle7;
 
+
     public Egg6(Needle7 needle7) {
         this.needle7 = needle7;
     }
+
 
     @Override
     public String toString() {
